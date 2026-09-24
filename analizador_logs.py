5c9e42ad2323d1b56e91a87ac67055051bf19384
GNsDD <danela.cuba946@gmail.com>
2026-09-24 16:42:05 -0500

    feat: entrega Actividad 4 - MapReduce analizador de logs

--- analizador_logs.py
+++ analizador_logs.py
@@ -13,7 +13,6 @@ class MRAnalizadorLogs(MRJob):
     def mapper(self, _, line):
         line_lower = line.lower()
         
-        # CASO 1: Filtrado de Logs de Error
         if "error" in line_lower:
             yield "LOG_ERROR_TOTAL", 1
         elif "critical" in line_lower:
@@ -23,7 +22,6 @@ class MRAnalizadorLogs(MRJob):
         for codigo in codigos_http:
             yield f"HTTP_STATUS_{codigo}", 1
 
-        # CASO 2: Procesamiento de palabras
         palabras = WORD_RE.findall(line_lower)
         for palabra in palabras:
             if palabra not in STOP_WORDS and not palabra.isdigit() and len(palabra) > 2:
